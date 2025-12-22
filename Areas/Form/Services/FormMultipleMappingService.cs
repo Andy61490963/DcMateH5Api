da2a38ac7492fb7017771bdbd440b47d8f47215c
Andy61490963 <lol901111@yahoo.com.tw>
2025-12-22 15:10:47 +0800

    移除多餘Model

--- Areas/Form/Services/FormMultipleMappingService.cs
+++ Areas/Form/Services/FormMultipleMappingService.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading;
 using ClassLibrary;
 using Dapper;
 using DcMateH5Api.Areas.Form.Interfaces;
--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace DcMateH5Api.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
